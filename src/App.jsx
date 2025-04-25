55657bf39d42b408fd6a6933595db76be59cc373
LinaFer29 <161323456+LinaFer29@users.noreply.github.com>
2025-04-25 11:02:11 -0500

    Creación Componente CharacterDetail

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Login from './components/Login/Login';
 import Layout from './components/Layout/Layout';
 import CharacterList from './components/Characters/CharacterList';
 import ContactForm from './components/Form/ContactForm';
+import CharacterDetail from './components/Characters/CharacterDetail';
 import './App.css';
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
             <Route path="/" element={<Layout />}>
               <Route index element={<Navigate to="/characters" replace />} />
               <Route path="characters" element={<CharacterList />} />
+              <Route path="/characters/:id" element={<CharacterDetail />} />
               <Route path="form" element={<ContactForm />} />
             </Route>
           </>
