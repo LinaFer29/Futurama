8e0acaf9bb73b226359794d47e2dd288e08cf4dd
LinaFer29 <161323456+LinaFer29@users.noreply.github.com>
2025-04-28 10:09:47 -0500

    Modificación Estilo Boton de Descarga

--- src/components/Layout/InstallButton.jsx
+++ src/components/Layout/InstallButton.jsx
@@ -45,17 +45,17 @@ function InstallButton() {
 
     return (
         <button
-            style={{
-                position: "fixed",
-                bottom: "20px",
-                right: "20px",
-                padding: "10px 20px",
-                backgroundColor: "#007bff",
-                color: "#fff",
-                border: "none",
-                borderRadius: "5px",
-                cursor: "pointer",
-            }}
+            // style={{
+            //     position: "fixed",
+            //     bottom: "20px",
+            //     right: "20px",
+            //     padding: "10px 20px",
+            //     backgroundColor: "#007bff",
+            //     color: "#fff",
+            //     border: "none",
+            //     borderRadius: "5px",
+            //     cursor: "pointer",
+            // }}
             onClick={handleInstallClick}
         >
             Download
